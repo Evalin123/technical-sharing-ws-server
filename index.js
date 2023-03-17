b373f87f8e76ff2f1baf43851ba3ad72ebfc982b
Eva Lin <evalin8@gmail.com>
2023-03-17 11:54:49 +0800

    update message

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const http = require("http");
+const moment = require("moment");
 const socketio = require("socket.io");
 
 const app = express();
@@ -12,18 +13,21 @@ const io = socketio(server);
 
 io.on("connection", (socket) => {
   /*只回傳給發送訊息的 client*/
-  socket.on("getMessage", (message) => {
-    socket.emit("getMessage", message);
+  socket.on("getMessage", (clientMsg) => {
+    const msg = `server: ${clientMsg}, time:${moment().valueOf()}`;
+    socket.emit("getMessage", msg);
   });
 
   /*回傳給所有連結著的 client*/
-  socket.on("getMessageAll", (message) => {
-    io.sockets.emit("getMessageAll", message); //io.sockets === io.of("/")
+  socket.on("getMessageAll", (clientMsg) => {
+    const msg = `server: ${clientMsg}, time:${moment().valueOf()}`;
+    io.sockets.emit("getMessageAll", msg); //io.sockets === io.of("/")
   });
 
   /*回傳給除了發送者外所有連結著的 client*/
-  socket.on("getMessageLess", (message) => {
-    socket.broadcast.emit("getMessageLess", message);
+  socket.on("getMessageLess", (clientMsg) => {
+    const msg = `server: ${clientMsg}, time:${moment().valueOf()}`;
+    socket.broadcast.emit("getMessageLess", msg);
   });
 
   socket.on("addRoom", (room) => {
@@ -38,12 +42,6 @@ io.on("connection", (socket) => {
     socket.join(room);
     socket.to(room).emit("addRoom", "已有新人加入聊天室！");
     io.sockets.in(room).emit("addRoom", "已加入聊天室！");
-  });
-
-  socket.on("send", (message) => {
-    if (message.room) {
-      io.sockets.in(message.room).emit("receivedMessage", `${message.message}`);
-    }
   });
 
   socket.on("disConnection", (message) => {
